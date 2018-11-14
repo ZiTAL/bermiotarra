f93c17ddfebb266b902f5494cfb8692bba10ead0
iban <iban@iban.eitb.eus>
2018-11-14 13:41:43 +0100

    pdf

--- web/private/constants.php
+++ web/private/constants.php
@@ -1,5 +1,5 @@
 <?php
 define('PUBLIC_ROOT', 'http://zital-pi.no-ip.org/bermiotarra');
-define('RELATIVE_ROOT', '/bermiotarra');
+define('RELATIVE_ROOT', '/bermiotarra/');
 define('TITLE', 'Bermiotarra');
 define('SEARCH_CAPTION', 'Bermiotarra: Bilatzailie');
