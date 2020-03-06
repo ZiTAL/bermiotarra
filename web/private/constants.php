240fcd6bb6ca1386aa5454a9d777800d3d91857c
zital <zitalman@gmail.com>
2020-03-06 20:11:37 +0100

    base kendu

--- web/private/constants.php
+++ web/private/constants.php
@@ -1,5 +1,10 @@
 <?php
-define('PUBLIC_ROOT', 'http://zital-pi.no-ip.org/bermiotarra');
+/*
+define('PUBLIC_ROOT', "http://bermiotarra.localhost");
+define('RELATIVE_ROOT', '/');
+*/
+define('PUBLIC_ROOT', "http://zital-pi.no-ip.org");
 define('RELATIVE_ROOT', '/bermiotarra/');
+
 define('TITLE', 'Bermiotarra');
 define('SEARCH_CAPTION', 'Bermiotarra: Bilatzailie');
