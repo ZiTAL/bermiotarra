43cddcb48c5d43c170c17f997e59c69f3404116b
bilbao_iban <bilbao_iban@eitb.eus>
2022-06-15 15:02:49 +0200

    typescript

--- web/public/search.js
+++ web/public/search.js
@@ -1,17 +1,23 @@
 "use strict";
 exports.__esModule = true;
 var http = require("http");
+var fs = require("fs");
+var jsdom_1 = require("jsdom");
+var Req;
+var Res;
 http.createServer(function (req, res) {
-    var params = parseGetParams(req.url);
-    var url = cleanUrl(req.url);
+    Req = req;
+    Res = res;
+    var params = parseGetParams(Req['url']);
+    var url = cleanUrl(Req['url']);
     switch (url) {
         case '/search':
+            search(params);
             break;
         default:
+            showError(404, 'Not Found!');
             break;
     }
-    res.writeHead(200);
-    res.end('Hello, World!');
 }).listen(8080);
 function parseGetParams(url) {
     var result = {};
@@ -24,11 +30,67 @@ function parseGetParams(url) {
     if (params.length > 0) {
         params.forEach(function (element) {
             var p = element.match(/([^=&]+)=([^=&]+)/);
-            result[p[1]] = p[2];
+            if (p)
+                result[p[1]] = p[2];
         });
     }
     return result;
 }
 function cleanUrl(url) {
     return url.replace(/\/?\?[^$]+$/, '');
+}
+function search(params) {
+    if (typeof params['q'] !== 'undefined') {
+        var dir_1 = '/home/projects/bermiotarra/web/public/berbak-esamoldiek/';
+        var files = fs.readdirSync(dir_1);
+        var found_1 = [];
+        files['forEach'](function (file) {
+            var html = fs.readFileSync(dir_1 + file, 'utf-8');
+            var dom = new jsdom_1.JSDOM(html);
+            var content = dom['window']['document']['body'].querySelector('div[id="content"]');
+            var childs = content['childNodes'];
+            var words = getWords(childs);
+            words.forEach(function (w) {
+                var t = '';
+                w.forEach(function (p) {
+                    t = t + p.textContent + "\n";
+                });
+                var r = new RegExp(decodeURI(params['q']), "gi");
+                console.log(t, r);
+                if (t.match(r)) {
+                    found_1.push(w);
+                }
+            });
+        });
+        console.log(found_1);
+    }
+    else
+        showError(400, 'Bad request!');
+}
+function getWords(childs) {
+    var deny_nodes = ['#text', 'H2'];
+    var words = [[]];
+    var i = -1;
+    childs.forEach(function (c) {
+        var node_name = c.nodeName;
+        if (deny_nodes.indexOf(node_name) !== -1)
+            return false;
+        if (node_name === 'H3') {
+            i++;
+            words[i] = [];
+        }
+        words[i].push(c);
+    });
+    return words;
+    /*
+      let t:string = ''
+      words[1].forEach(function(c)
+      {
+        t = t+c.textContent+"\n"
+      })
+    */
+}
+function showError(code, text) {
+    Res['writeHead'](code);
+    Res['end'](text);
 }
--- web/public/search.ts
+++ web/public/search.ts
@@ -1,20 +1,27 @@
-import * as http from "http"
+import * as http    from 'http'
+import * as fs      from 'fs'
+import { JSDOM }    from 'jsdom'
 
-http.createServer(function (req, res)
+let Req:object
+let Res:object
+
+http.createServer(function (req:object, res:object)
 {
-  const params:object  = parseGetParams(req.url)
-  const url:string     = cleanUrl(req.url)
+  Req = req
+  Res = res
+
+  const params:object  = parseGetParams(Req['url'])
+  const url:string     = cleanUrl(Req['url'])
 
   switch(url)
   {
     case '/search':
+      search(params)
       break
     default:
+      showError(404, 'Not Found!')
       break
   }
-
-  res.writeHead(200);
-  res.end('Hello, World!');
 }).listen(8080);
 
 function parseGetParams(url:string):object
@@ -33,7 +40,8 @@ function parseGetParams(url:string):object
     params.forEach(function(element)
     {
       let p         = element.match(/([^=&]+)=([^=&]+)/)
-      result[p[1]]  = p[2]
+      if(p)
+        result[p[1]]  = p[2]
     })
   }
 
@@ -43,4 +51,76 @@ function parseGetParams(url:string):object
 function cleanUrl(url:string):string
 {
   return url.replace(/\/?\?[^$]+$/, '')
+}
+
+function search(params:object)
+{
+  if(typeof params['q'] !== 'undefined')
+  {
+    const dir:string        = '/home/projects/bermiotarra/web/public/berbak-esamoldiek/'
+    const files:object      = fs.readdirSync(dir)
+
+    let found = []
+    files['forEach'](function(file)
+    {
+      const html:string     = fs.readFileSync(dir+file, 'utf-8')
+      const dom:object      = new JSDOM(html);
+      const content:object  = dom['window']['document']['body'].querySelector('div[id="content"]')
+      const childs:object   = content['childNodes']
+      
+      let words = getWords(childs)
+      
+      words.forEach(function(w)
+      {
+        let t:string = ''
+        w.forEach(function(p)
+        {
+          t = t+p.textContent+"\n"
+        })
+        const r = new RegExp(decodeURI(params['q']), "gi");
+        console.log(t, r)
+        if(t.match(r))
+        {
+          found.push(w)
+        }
+      })
+    })
+    console.log(found);
+  }
+  else
+    showError(400, 'Bad request!')
+}
+
+function getWords(childs)
+{
+  let deny_nodes = ['#text', 'H2']
+  let words = [[]]
+  let i     = -1
+  childs.forEach(function(c)
+  {
+    const node_name = c.nodeName
+    if(deny_nodes.indexOf(node_name)!==-1)
+      return false;
+
+    if(node_name==='H3')
+    {
+      i++;
+      words[i] = []
+    }          
+    words[i].push(c)
+  })
+  return words
+/*
+  let t:string = ''
+  words[1].forEach(function(c)
+  {
+    t = t+c.textContent+"\n"
+  })
+*/
+}
+
+function showError(code:number, text:string)
+{
+  Res['writeHead'](code)
+  Res['end'](text)
 }
