578064a4b54b458e16255e5abe6fa0dd871144f8
bilbao_iban <bilbao_iban@eitb.eus>
2022-08-09 10:11:58 +0200

    update.sh

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 git pull
 cd web/private
 
-ncc build ./build.ts -o dist/build
-ncc build ./search.ts -o dist/search
-ncc build ./server.ts -o dist/server
+ncc build ./build.ts    -o dist/build
+ncc build ./search.ts   -o dist/search
+ncc build ./server.ts   -o dist/server
 
 pm2 stop bermiotarra
 pm2 start dist/server/index.js --name bermiotarra
