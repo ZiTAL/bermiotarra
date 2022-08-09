06fbc1301a6696047b31da305bba38bbbc55726a
bilbao_iban <bilbao_iban@eitb.eus>
2022-08-09 14:40:42 +0200

    header

--- web/private/lib/constants.ts
+++ web/private/lib/constants.ts
@@ -3,5 +3,6 @@ module.exports =
     PUBLIC_ROOT:    'http://zital-pi.no-ip.org',
     RELATIVE_ROOT:  '/bermiotarra/',
     TITLE:          'Bermiotarra',
+    HEADER:         'Bermiotarra',
     SEARCH_CAPTION: 'Bermiotarra: Bilatzailie'
 }
