c0f19fa44107d942e39f6c5e0ebea394313c563f
bilbao_iban <bilbao_iban@eitb.eus>
2022-08-09 13:18:12 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git pull
 cd web/private
 
 ncc build ./build.ts    -o dist/build
-ncc build ./search.ts   -o dist/search
+#ncc build ./search.ts   -o dist/search
 ncc build ./server.ts   -o dist/server
 
 node dist/build/index.js
