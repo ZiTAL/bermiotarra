f85e5131fa179016c5574ac63b04c50a5a0360f3
bilbao_iban <bilbao_iban@eitb.eus>
2024-11-05 10:46:34 +0100

    deploy argument build

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 git pull
 cd web/private
 
-ncc build ./build.ts    -o dist/build
-#ncc build ./search.ts   -o dist/search
-ncc build ./server.ts   -o dist/server
+if [ "$1" == "build" ]; then
+    ncc build ./build.ts    -o dist/build
+    ncc build ./search.ts   -o dist/search
+    ncc build ./server.ts   -o dist/server
+fi
 
 node dist/build/index.js
 
