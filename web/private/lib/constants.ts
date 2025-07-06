20a5657389520d02492c72ce307aff70215939d1
zital <zital@riseup.net>
2025-07-06 15:10:55 +0200

    html seo

--- web/private/lib/constants.ts
+++ web/private/lib/constants.ts
@@ -3,6 +3,7 @@ module.exports =
     PUBLIC_ROOT:    'https://bermiotarra.zital.eus',
     RELATIVE_ROOT:  '/',
     TITLE:          'Bermiotarra',
+    DESCRIPTION:    'Bermioko berba eta esamoldien hiztegi librie',
     HEADER:         'Bermiotarra',
     SEARCH_CAPTION: 'Bermiotarra: Bilatzailie'
 }
