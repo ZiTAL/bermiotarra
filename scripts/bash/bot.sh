6fda44ae8cbeb338d38015312fb0f72d89875d69
zital <zital@riseup.net>
2026-07-12 13:33:44 +0200

    docker

--- scripts/bash/bot.sh
+++ scripts/bash/bot.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 /usr/bin/podman start docker_python_1
-/usr/bin/podman exec -it docker_python_1 python3 /app/bot/bot.py
+/usr/bin/podman exec docker_python_1 python3 /app/bot/bot.py
 /usr/bin/podman stop docker_python_1
